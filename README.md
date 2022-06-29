153671f3553c340e5c1b4cdc4a4b467c481066fd
深澤 康介 <kousuke@morishimanoMacBook-Pro-2.local>
2022-06-29 21:57:10 +0900

    add gif and support for pkl file

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # smpl2bvh
 This repository contains an example script to convert from a SMPL model to a bvh file.
 
+<img src="gif/aistpp.gif" align="center"> <br>
+
 ## Notification
 This code is MIT licensed, but SMPL requires a separate license.  
 Please see [SMPL official website](https://smpl.is.tue.mpg.de/).
@@ -19,11 +21,16 @@ After downloads all requirements, you can use smpl2bvh like this:
     python smpl2bvh.py --gender MALE --poses data/poses.npz --fps 30 --output data/demo.bvh
 ```
 
-`poses` is an npz file, which must contain `rotations` and `trans` as keys.  
-`rotations` value is an array consisting of [fnum, 24, 3] and `trans` value is the root transition consisting of [fnum, 3]
+`poses` is an `.npz` file or `.pkl` file.  
+`npz` file must contain `rotations` and `trans` as keys.  
+`rotations` value is an np.array consisting of [fnum, 24, 3] and `trans` value is the root transition consisting of [fnum, 3]
 (fnum means frame number).  
-You can find bvh file as `output`.  
-For more information, plese refer to `smpl2bvh.py`.  
+
+`pkl` file must contain `smpl_poses` and `smpl_scaling` and `smpl_trans` as keys.  
+`smpl_poses` value is an np.array consisting of [fnum, 72] and `smpl_scaling` value is the scaling parameter. `smpl_trans` value is the root transition consisting of [fnum, 3].  
+The format of `pkl` file is the same as [AIST++](https://google.github.io/aistplusplus_dataset/) dataset.  
+You can find bvh file as `--output`.  
+For more information, please refer to `smpl2bvh.py`.  
 
 ## Reference
 `bvh.py` and `quat.py` are based on [Motion Matching](https://github.com/orangeduck/Motion-Matching).
--- smpl2bvh.py
+++ smpl2bvh.py
@@ -1,72 +1,102 @@
 import torch
 import numpy as np
 import argparse
+import pickle
 import smplx
 
 from utils import bvh, quat
 
-names = [
-    "pelvis",
-    "left_hip",
-    "right_hip",
-    "spine1",
-    "left_knee",
-    "right_knee",
-    "spine2",
-    "left_ankle",
-    "right_ankle",
-    "spine3",
-    "left_foot",
-    "right_foot",
-    "neck",
-    "left_collar",
-    "right_collar",
-    "head",
-    "left_shoulder",
-    "right_shoulder",
-    "left_elbow",
-    "right_elbow",
-    "left_wrist",
-    "right_wrist",
-    "left_palm",
-    "right_palm",
-]
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--model_path", type=str, default="data/smpl/")
-    parser.add_argument("--model_type", type=str, default="smpl")
-    parser.add_argument("--gender", type=str, default="MALE")
-    parser.add_argument("--num_betas", type=int, default=10)
+    parser.add_argument("--model_type", type=str, default="smpl", choices=["smpl", "smplx"])
+    parser.add_argument("--gender", type=str, default="MALE", choices=["MALE", "FEMALE", "NEUTRAL"])
+    parser.add_argument("--num_betas", type=int, default=10, choices=[10, 300])
     parser.add_argument("--poses", type=str, default="data/demo.npz")
     parser.add_argument("--fps", type=int, default=30)
     parser.add_argument("--output", type=str, default="data/demo_smpl.bvh")
     return parser.parse_args()
 
+def smpl2bvh(model_path:str, poses:str, output:str, 
+             model_type="smpl", gender="MALE", num_betas=10, fps=30)->None:
+    """Save bvh file created by smpl parameters.
 
-if __name__ == "__main__":
-    args = parse_args()
+    Args:
+        model_path (str): Path to smpl models.
+        poses (str): Path to npz or pkl file.
+        output (str): Where to save bvh.
+        model_type (str, optional): I prepared "smpl" only. Defaults to "smpl".
+        gender (str, optional): Gender Information. Defaults to "MALE".
+        num_betas (int, optional): How many pca parameters to use in SMPL. Defaults to 10.
+        fps (int, optional): Frame per second. Defaults to 30.
+    """
+    
+    names = [
+        "pelvis",
+        "left_hip",
+        "right_hip",
+        "spine1",
+        "left_knee",
+        "right_knee",
+        "spine2",
+        "left_ankle",
+        "right_ankle",
+        "spine3",
+        "left_foot",
+        "right_foot",
+        "neck",
+        "left_collar",
+        "right_collar",
+        "head",
+        "left_shoulder",
+        "right_shoulder",
+        "left_elbow",
+        "right_elbow",
+        "left_wrist",
+        "right_wrist",
+        "left_palm",
+        "right_palm",
+    ]
     
     # I prepared smpl models only, but I will release for smplx models recently.
-    model = smplx.create(model_path=args.model_path, 
-                        model_type=args.model_type,
-                        gender=args.gender, 
+    model = smplx.create(model_path=model_path, 
+                        model_type=model_type,
+                        gender=gender, 
                         batch_size=1)
     
     parents = model.parents.detach().cpu().numpy()
     
     # You can define betas like this.(default betas are 0 at all.)
     rest = model(
-        # betas = torch.randn([1, model.num_betas], dtype=torch.float32)
+        # betas = torch.randn([1, num_betas], dtype=torch.float32)
     )
     rest_pose = rest.joints.detach().cpu().numpy().squeeze()[:24,:]
     
     offsets = rest_pose - rest_pose[parents]
     offsets[0] = np.zeros(3)
     
-    poses = np.load(args.poses)
-    rots = np.squeeze(poses["poses"], axis=0)
-    trans = np.squeeze(poses["trans"], axis=0)
+    scaling = None
+    
+    # Pose setting.
+    if poses.endswith(".npz"):
+        poses = np.load(poses)
+        rots = np.squeeze(poses["poses"], axis=0) # (N, 24, 3)
+        trans = np.squeeze(poses["trans"], axis=0) # (N, 3)
+
+    elif poses.endswith(".pkl"):
+        with open(poses, "rb") as f:
+            poses = pickle.load(f)
+            rots = poses["smpl_poses"] # (N, 72)
+            rots = rots.reshape(rots.shape[0], -1, 3) # (N, 24, 3)
+            scaling = poses['smpl_scaling']  # (1,)
+            trans = poses['smpl_trans']  # (N, 3)
+    
+    else:
+        raise Exception("This file type is not supported!")
+    
+    if scaling is not None:
+        trans /= scaling
     
     order = "zyx"
     positions = offsets[None].repeat(len(rots), axis=0)
@@ -84,7 +114,13 @@ if __name__ == "__main__":
         "parents": parents,
         "names": names,
         "order": order,
-        "frametime": 1 / args.fps,
+        "frametime": 1 / fps,
     }
     
-    bvh.save(args.output, bvh_data)
+    bvh.save(output, bvh_data)
+
+if __name__ == "__main__":
+    args = parse_args()
+    
+    smpl2bvh(model_path=args.model_path, model_type=args.model_type, gender=args.gender,
+             poses=args.poses, num_betas=args.num_betas, fps=args.fps, output=args.output)
